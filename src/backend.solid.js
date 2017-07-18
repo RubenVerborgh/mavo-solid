b7bf6fa56c91cde459f6a55aca0c538dc37cd519
Ruben Verborgh <ruben@verborgh.org>
2017-07-18 16:07:04 -0400

    Add back-end stub.

---
+++ src/backend.solid.js
@@ -0,0 +1,16 @@
+(function ($) {
+const _ = Mavo.Backend.register($.Class({
+	extends: Mavo.Backend,
+	id: 'Solid',
+	constructor: function () {
+		console.log(`Solid: created backend from ${this.source} for ${this.mavo.id}.`);
+	},
+
+	static: {
+		test: function (source) {
+			// TODO: Add more reliable test (https://github.com/solid/mavo-solid/issues/1)
+			return /^https:\/\/[^/]+\.databox\.me/.test(source);
+		},
+	},
+}));
+})(Bliss);
